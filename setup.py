a8feb3b5adefdf60f8db56acdd865ff16b5163ba
monsterxcn <monsterxcn@gmail.com>
2022-04-02 10:22:10 +0800

    :bug: Pillow 依赖缺失 (#2)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-color",
-    version="0.1.0",
+    version="0.1.1",
     author="monsterxcn",
     author_email="monsterxcn@gmail.com",
     description="A specified color image generator for Nonebot2",
@@ -24,6 +24,7 @@ setuptools.setup(
     python_requires=">=3.7.3,<4.0",
     install_requires=[
         'nonebot2>=2.0.0a14',
-        'nonebot-adapter-onebot>=2.0.0b1'
+        'nonebot-adapter-onebot>=2.0.0b1',
+        'Pillow>=8.2.0'
     ],
 )
